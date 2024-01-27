ce82bb8aeae2e816c3533692847d7d2b2cfa9dfc
raghul <raghulkrishna50@gmail.com>
2024-01-27 18:51:18 +0530

    test : pushing

---
+++ src/components/Comp.tsx
@@ -0,0 +1,9 @@
+const Comp = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center text-3xl">
+      hey there this is viji tours and travels here
+    </div>
+  );
+};
+
+export default Comp;
