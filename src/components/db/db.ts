24d52751a8206e8ef29ed9f488752164826c653c
raghul <raghulkrishna50@gmail.com>
2024-03-08 17:24:28 +0530

    check: deploy-2

--- src/components/db/db.ts
+++ src/components/db/db.ts
@@ -2,7 +2,7 @@ import pg from 'pg'
 
 const db = new pg.Client({
     user: process.env.userdb,
-    // host: process.env.host,
+    host: process.env.host || "3000",
     database: process.env.database,
     password: process.env.dbpass,
     port: 5432
