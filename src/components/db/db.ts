a317d6d5a38ded8c6739981489cb9477bc45d49a
raghul <raghulkrishna50@gmail.com>
2024-03-11 21:48:52 +0530

    check : deployment

--- src/components/db/db.ts
+++ src/components/db/db.ts
@@ -1,12 +1,12 @@
-import pg from 'pg'
+// import pg from 'pg'
 
-const db = new pg.Client({
-    user: process.env.userdb,
-    host: "localhost",
-    database: process.env.database,
-    password: process.env.dbpass,
-    port: 5432
-})
-db.connect();
+// const db = new pg.Client({
+//     user: process.env.userdb,
+//     host: "localhost",
+//     database: process.env.database,
+//     password: process.env.dbpass,
+//     port: 5432
+// })
+// db.connect();
 
-export default db;
+// export default db;
